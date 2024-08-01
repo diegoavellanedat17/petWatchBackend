ade2e045c111bb2310d3b13781881447380d5d0f
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-08-01 09:47:12 -0500

    cors conofiguration changed

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,7 @@ import { initDatabase } from "./models/coordinateModel";
 const app = express();
 const port = 3000;
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use("/api", coordinateRoutes);
