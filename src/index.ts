76d377c310b5dc70bc0186bae77ccb3acc49a957
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-08-01 09:13:50 -0500

    back to http to server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import fs from "fs";
-import https from "https";
 import coordinateRoutes from "./routes/coordinateRoutes";
 import { initDatabase } from "./models/coordinateModel";
 
@@ -20,14 +18,7 @@ app.use(
 app.use(bodyParser.json());
 app.use("/api", coordinateRoutes);
 
-const privateKey = fs.readFileSync("key.pem", "utf8");
-const certificate = fs.readFileSync("cert.pem", "utf8");
-
-const credentials = { key: privateKey, cert: certificate };
-
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, async () => {
+app.listen(port, async () => {
   console.log(`HTTPS Server running on https://localhost:${port}`);
   await initDatabase();
   console.log("Database initialized");
