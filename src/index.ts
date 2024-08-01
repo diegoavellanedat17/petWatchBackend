02e0443b8935e31db29bc37b7654bc4632b53317
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-08-01 08:26:56 -0500

    use the certificate for https

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import fs from "fs";
+import https from "https";
 import coordinateRoutes from "./routes/coordinateRoutes";
 import { initDatabase } from "./models/coordinateModel";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.use(bodyParser.json());
-
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
 app.use("/api", coordinateRoutes);
 
-app.listen(port, async () => {
-  console.log(`Server running on http://localhost:${port}`);
+const privateKey = fs.readFileSync("key.pem", "utf8");
+const certificate = fs.readFileSync("cert.pem", "utf8");
+
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, async () => {
+  console.log(`HTTPS Server running on https://localhost:${port}`);
   await initDatabase();
   console.log("Database initialized");
 });
